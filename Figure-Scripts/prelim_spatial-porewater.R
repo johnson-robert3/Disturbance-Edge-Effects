98828aecd41001d67b833f59321314a5f09bcceb
Robert Johnson <johnson.robert.333@gmail.com>
2025-10-10 17:09:38 -0500

    update figs now that there are site 3 data and rhizome depth data

--- Figure-Scripts/prelim_spatial-porewater.R
+++ Figure-Scripts/prelim_spatial-porewater.R
@@ -12,7 +12,7 @@ fk_shapes = c('disturbed' = 22, 'undisturbed' = 15)
 # box plots by site (pooled patches)
 windows(height=3.5, width=5)
 ggplot(fk_pw_spatial %>%
-          mutate(site = factor(site, levels = c('1', '2')))) +
+          mutate(site = factor(site, levels = c('1', '2', '3')))) +
    #
    geom_boxplot(aes(x = site, y = porewater_S_uM, group = interaction(site, treatment), color = treatment)) +
    # 
@@ -98,12 +98,13 @@ windows(height=3.5, width=8)
 ggplot(fk_pw_spatial %>%
           mutate(transect_location_m = if_else(treatment=="disturbed", transect_location_m * -1, transect_location_m))) +
    #
-   geom_line(aes(x = transect_location_m, y = porewater_S_uM, group = interaction(site_id, treatment), linetype = treatment, color = site_id), alpha=0.5) +
+   geom_line(aes(x = transect_location_m, y = porewater_S_uM, group = interaction(site_id, treatment, sample_depth), linetype = treatment, color = site_id), alpha=0.5) +
    geom_smooth(aes(x = transect_location_m, y = porewater_S_uM, group = treatment), color="black", linewidth=1.5) +
-   geom_point(aes(x = transect_location_m, y = porewater_S_uM, fill = site_id), shape = 21, size=3, alpha = 0.5) +
+   geom_point(aes(x = transect_location_m, y = porewater_S_uM, fill = site_id, shape = sample_depth), size=3, alpha = 0.5) +
    geom_vline(aes(xintercept = 0), linetype=2, color="gray50") +
    #
    scale_y_continuous(name = expression(Porewater~sulfide~(mu*M))) +
+   scale_shape_manual(values = c('surface' = 21, 'rhizome' = 25)) +
    facet_wrap(facets = vars(site), labeller = label_both) +
    #
    theme_classic() +
