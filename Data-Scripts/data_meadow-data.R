2e7c451fd6ca5bd028d861f829ac75457d1c9846
Robert Johnson <johnson.robert.333@gmail.com>
2025-10-21 14:23:04 -0500

    minor updates and comments; fixed issue in creating biomass df

--- Data-Scripts/data_meadow-data.R
+++ Data-Scripts/data_meadow-data.R
@@ -17,50 +17,64 @@ raw_bps = read_csv("Data/FLK24_thalassia_blades_per_shoot.csv")
 raw_ag_biomass = read_csv("Data/FLK24_aboveground_biomass.csv")
 
 
-# seagrass
+#- Seagrass
+
+# areal density of variables (seagrass, macroalgae, burrows, etc.)
 density = raw_meadow_parameters %>%
    rename(Tt_seed_count = Tt_seed) %>%
    mutate(across(ends_with("count"), ~./quadrat_area_m2)) %>%
    rename_with(.cols = ends_with("count"), .fn = ~str_replace(., pattern = "count", replacement = "density")) %>%
-   mutate(total_ma_density = rowSums(select(., Hal_density, Udo_density, Pen_density)))
+   mutate(total_ma_density = rowSums(select(., Hal_density, Udo_density, Pen_density, Rhi_density, Cau_density), na.rm=TRUE))
 
+# mean blade length and width for each quadrat (morph measured on 5 blades from each quadrat)
 morph = raw_morphometry %>%
    summarize(blade_length = mean(blade_length_cm, na.rm=TRUE),
              blade_width = mean(blade_width_cm, na.rm=TRUE),
              .by = c(site_id, treatment, transect_location_m))
 
+# mean blades per shoot for each quadrat (BPS counted on 5 shoots from each quadrat
 bps = raw_bps %>%
    summarize(bps = mean(Tt_bps, na.rm=TRUE), .by = c(site_id, treatment, transect_location_m))
 
+# areal aboveground biomass
 biomass = raw_ag_biomass %>%
    janitor::remove_empty(which = "rows") %>%
+   # areal dry biomass
    mutate(biomass = (dry_mass_g - dish_mass_g) / quadrat_area_m2) %>%
+   # for empty quadrats (no biomass), NAs were entered, replace these with Tt for species so the rows are retained
    mutate(species = replace_na(species, "T. testudinum")) %>%
-   summarize(biomass = sum(biomass), .by = c(site_id, treatment, transect_location_m, species)) %>%
+   select(site_id, treatment, transect_location_m, species, biomass) %>%
    pivot_wider(id_cols = c(site_id, treatment, transect_location_m),
                names_from = species,
                values_from = biomass) %>%
    rename(Tt_biomass = "T. testudinum",
           Hw_biomass = "H. wrightii",
+          Sf_biomass = "S. filiforme",
           Hal_biomass = "Halimeda spp.",
           Pen_biomass = "Penicillus spp.",
+          Rhi_biomass = "Rhipocephalus spp.",
           drift_biomass = "Drift algae",
           Tt_seed_biomass = "Thalassia seed") %>%
-   mutate(total_ma_biomass = rowSums(select(., Hal_biomass, Pen_biomass), na.rm=TRUE))
+   relocate(Hw_biomass, Sf_biomass, .after = Tt_biomass) %>%
+   # total seagrass biomass and total macroalgal biomass in each quadrat
+   mutate(total_sg_biomass = rowSums(select(., Tt_biomass, Hw_biomass, Sf_biomass), na.rm=TRUE),
+          total_ma_biomass = rowSums(select(., Hal_biomass, Pen_biomass, Rhi_biomass), na.rm=TRUE))
 
 
-# sediment
+#- Sediment
 sed = raw_spatial_dbd %>%
    mutate(dry_mass = dry_mass_g - wp_mass_g,
           wet_mass = wet_mass_g - wp_mass_g,
+          # dry bulk density
           dbd = dry_mass / sample_vol_cc,
+          # seawater density = 1.02 g/cc
           porosity = ((wet_mass - dry_mass) / 1.02) / sample_vol_cc,
           om_drymass = dry_sample_g - crucible_mass_g,
           om_ashmass = ashed_mass_g - crucible_mass_g,
           perc_om = (om_drymass - om_ashmass) / om_drymass * 100)
 
 
-# combine
+#- Combined meadow parameter dataset
 meadow = density %>%
    select(site_id, site, patch, treatment, transect_location_m, contains("density")) %>%
    full_join(morph) %>%
