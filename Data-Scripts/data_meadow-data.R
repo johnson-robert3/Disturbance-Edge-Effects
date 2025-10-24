bfa86d2dd0d235eece3e9d8aa9dec8033c58b69d
Robert Johnson <johnson.robert.333@gmail.com>
2025-10-24 10:55:39 -0500

    create combined df w/ meadow and porewater data; add rhizome df

--- Data-Scripts/data_meadow-data.R
+++ Data-Scripts/data_meadow-data.R
@@ -15,6 +15,9 @@ raw_meadow_parameters = read_csv("Data/FLK24_meadow_parameters.csv")
 raw_morphometry = read_csv("Data/FLK24_thalassia_morphometry.csv")
 raw_bps = read_csv("Data/FLK24_thalassia_blades_per_shoot.csv")
 raw_ag_biomass = read_csv("Data/FLK24_aboveground_biomass.csv")
+raw_rhizomes = read_csv("Data/FLK24_rhizomes.csv")
+
+site_data = read_csv("Data/FLK24_site_data.csv")
 
 
 #- Seagrass
@@ -22,12 +25,15 @@ raw_ag_biomass = read_csv("Data/FLK24_aboveground_biomass.csv")
 # areal density of variables (seagrass, macroalgae, burrows, etc.)
 density = raw_meadow_parameters %>%
    rename(Tt_seed_count = Tt_seed) %>%
+   # density (units = shoots / m2)
    mutate(across(ends_with("count"), ~./quadrat_area_m2)) %>%
    rename_with(.cols = ends_with("count"), .fn = ~str_replace(., pattern = "count", replacement = "density")) %>%
+   # total macroalgal density
    mutate(total_ma_density = rowSums(select(., Hal_density, Udo_density, Pen_density, Rhi_density, Cau_density), na.rm=TRUE))
 
 # mean blade length and width for each quadrat (morph measured on 5 blades from each quadrat)
 morph = raw_morphometry %>%
+   # (units = cm)
    summarize(blade_length = mean(blade_length_cm, na.rm=TRUE),
              blade_width = mean(blade_width_cm, na.rm=TRUE),
              .by = c(site_id, treatment, transect_location_m))
@@ -39,7 +45,7 @@ bps = raw_bps %>%
 # areal aboveground biomass
 biomass = raw_ag_biomass %>%
    janitor::remove_empty(which = "rows") %>%
-   # areal dry biomass
+   # areal dry biomass (units = g DM / m2)
    mutate(biomass = (dry_mass_g - dish_mass_g) / quadrat_area_m2) %>%
    # for empty quadrats (no biomass), NAs were entered, replace these with Tt for species so the rows are retained
    mutate(species = replace_na(species, "T. testudinum")) %>%
@@ -61,11 +67,34 @@ biomass = raw_ag_biomass %>%
           total_ma_biomass = rowSums(select(., Hal_biomass, Pen_biomass, Rhi_biomass), na.rm=TRUE))
 
 
+# Rhizomes
+rhizomes = raw_rhizomes %>%
+   # convert NAs to 0 for cores w/o rhizomes
+   mutate(across(contains("mass_g"), ~replace_na(., 0))) %>%
+   # areal rhizome biomass (units = g DM / m2)
+   mutate(core_area = (pi * (core_diameter_cm^2)) / 10000,
+          bg_biomass = (dry_mass_g - dish_mass_g) / core_area) %>%
+   # fix the instance where the bottom of the rhizome layer is listed as 0cm, (no rhizomes in core, so should be NA)
+   mutate(rhizome_bottom_cm = na_if(rhizome_bottom_cm, 0))
+
+
+## Combined seagrass parameter dataset
+seagrass = density %>%
+   select(site_name, site_id, site, patch, treatment, transect_location_m, contains("density")) %>%
+   full_join(morph) %>%
+   full_join(bps) %>%
+   full_join(biomass) %>%
+   # add one-sided leaf area index for Thalassia (units = m2 / m2)
+   mutate(lai = (Tt_density * blade_length * blade_width * bps) / 10000) %>%
+   full_join(rhizomes %>%
+                select(site_id, treatment, transect_location_m, rhizome_top_cm, rhizome_bottom_cm, bg_biomass))
+
+
 #- Sediment
 sed = raw_spatial_dbd %>%
    mutate(dry_mass = dry_mass_g - wp_mass_g,
           wet_mass = wet_mass_g - wp_mass_g,
-          # dry bulk density
+          # dry bulk density (units = g / cm3)
           dbd = dry_mass / sample_vol_cc,
           # seawater density = 1.02 g/cc
           porosity = ((wet_mass - dry_mass) / 1.02) / sample_vol_cc,
@@ -74,17 +103,22 @@ sed = raw_spatial_dbd %>%
           perc_om = (om_drymass - om_ashmass) / om_drymass * 100)
 
 
-#- Combined meadow parameter dataset
-meadow = density %>%
-   select(site_id, site, patch, treatment, transect_location_m, contains("density")) %>%
-   full_join(morph) %>%
-   full_join(bps) %>%
-   full_join(biomass) %>%
+#- Combined meadow parameter dataset (combine seagrass, sediment, and porewater)
+meadow = seagrass %>%
    full_join(sed %>%
                 select(site_id, treatment, transect_location_m, dbd, porosity, perc_om)) %>%
-   mutate(transect_location_m = if_else(treatment=="disturbed", transect_location_m * -1, transect_location_m),
-          lai = (Tt_density * blade_length * blade_width * bps) / 10000)
-
+   full_join(fk_pw_spatial %>%
+                select(site_id:sample_depth, porewater_S_uM) %>%
+                # widen dataset to make surface and rhizome measurements their own columns (i.e., create one row per sample ID)
+                pivot_wider(id_cols = c(site_id:transect_location_m),
+                            names_from = sample_depth,
+                            values_from = porewater_S_uM) %>%
+                rename(surface_pwS = surface,
+                       rhizome_pwS = rhizome) %>%
+                # update treatment levels to match the seagrass and sediment datasets 
+                #  (SG & Sed CSV files had treatment terminology changed later, from dist/undist to unveg/veg)
+                mutate(treatment = case_when(treatment == "disturbed" ~ "unvegetated",
+                                             treatment == "undisturbed" ~ "vegetated")))
 
 
 
