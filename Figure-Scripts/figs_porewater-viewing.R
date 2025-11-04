5f57ee20e3618ec913f3ace29298ee52074d95a7
Robert Johnson <johnson.robert.333@gmail.com>
2025-11-04 15:27:37 -0600

    add figs with log transformed sulfide values, add figs of vars vs distance to edge from other data script

--- Figure-Scripts/figs_porewater-viewing.R
+++ Figure-Scripts/figs_porewater-viewing.R
@@ -28,7 +28,20 @@ ggplot(meadow) +
    #
    theme_classic() +
    theme(panel.border = element_rect(color="black", fill=NA))
-   
+
+      # log
+      windows(height=3.5, width=5)
+      ggplot(meadow) +
+         #
+         geom_line(aes(x = distance, y = log(surface_pwS), group = interaction(site_id, treatment), linetype = treatment, color = site_id)) +
+         geom_point(aes(x = distance, y = log(surface_pwS), fill = site_id), shape = 21, size=3) +
+         geom_vline(aes(xintercept = 0), linetype=2, color="gray50") +
+         #
+         scale_y_continuous(name = expression(ln~Porewater~sulfide~(mu*M))) +
+         #
+         theme_classic() +
+         theme(panel.border = element_rect(color="black", fill=NA))
+  
 
 # Surface PW S based on distance from edge, by site
 windows(height=3.5, width=8)
@@ -62,7 +75,47 @@ ggplot(meadow) +
    #
    theme_classic() +
    theme(panel.border = element_rect(color="black", fill=NA))
-   
+
+      # log
+      windows(height=3.5, width=8)
+      ggplot(meadow) +
+         #
+         geom_line(aes(x = distance, y = log(surface_pwS), group = interaction(site_id, treatment), linetype = treatment), 
+                   linewidth= 0.75, alpha=0.5) +
+         geom_point(aes(x = distance, y = log(surface_pwS)), size=3, alpha = 0.5) +
+         geom_vline(aes(xintercept = 0), linetype=2, color="gray50") +
+         #
+         scale_y_continuous(name = expression(ln~Surface~sulfide~(mu*M))) +
+         facet_wrap(facets = vars(site_id), nrow=2) +
+         #
+         theme_classic() +
+         theme(panel.border = element_rect(color="black", fill=NA))
+
+
+
+# Surface sulfide vs distance from the veg edge
+#  all data pooled, separated by treatment
+ggplot(meadow) +
+   geom_point(aes(x = transect_location_m, y = surface_pwS, color = treatment), position = position_jitter(height=0, width=0.07)) + 
+   geom_smooth(aes(x = transect_location_m, y = surface_pwS, color = treatment), method="lm", se=F)
+
+# log transform
+ggplot(meadow) +
+   geom_point(aes(x = transect_location_m, y = log(surface_pwS), color = treatment), position = position_jitter(height=0, width=0.07)) + 
+   geom_smooth(aes(x = transect_location_m, y = log(surface_pwS), color = treatment), method="lm", se=F)
+
+
+# with separate lines by treatment
+#  all data pooled, separated by site and treatment
+ggplot(meadow) +
+   geom_point(aes(x = transect_location_m, y = surface_pwS, color = treatment), position = position_jitter(height=0, width=0.07)) + 
+   geom_smooth(aes(x = transect_location_m, y = surface_pwS, color = treatment, group=interaction(treatment, site_name), linetype=site_name), method="lm", se=F)
+
+# log transform
+ggplot(meadow) +
+   geom_point(aes(x = transect_location_m, y = log(surface_pwS), color = treatment), position = position_jitter(height=0, width=0.07)) + 
+   geom_smooth(aes(x = transect_location_m, y = log(surface_pwS), color = treatment, group=interaction(treatment, site_name), linetype=site_name), method="lm", se=F)
+
 
 
 #-- Rhizome Porewater S viewed along transects --#
@@ -98,6 +151,21 @@ ggplot(meadow) +
    theme_classic() +
    theme(panel.border = element_rect(color="black", fill=NA))
 
+      # log
+      windows(height=3.5, width=8)
+      ggplot(meadow) +
+         #
+         geom_line(aes(x = distance, y = log(rhizome_pwS), group = interaction(site_id, treatment), linetype = treatment), 
+                   linewidth= 0.75, alpha=0.5) +
+         geom_point(aes(x = distance, y = log(rhizome_pwS)), size=3, alpha = 0.5) +
+         geom_vline(aes(xintercept = 0), linetype=2, color="gray50") +
+         #
+         scale_y_continuous(name = expression(ln~Rhizome~sulfide~(mu*M))) +
+         facet_wrap(facets = vars(site_id), nrow=2) +
+         #
+         theme_classic() +
+         theme(panel.border = element_rect(color="black", fill=NA))
+      
 
 # Rhizome porewater based on distance from edge, but treatments separated (by patch and treatment)
 windows(height=3.5, width=8)
@@ -119,18 +187,44 @@ ggplot(meadow) +
    theme(panel.border = element_rect(color="black", fill=NA))
 
 
+# Rhizome sulfide vs distance from the veg edge
+#  all data pooled, separated by treatment
+ggplot(meadow) +
+   geom_point(aes(x = transect_location_m, y = rhizome_pwS, color = treatment), position = position_jitter(height=0, width=0.07)) + 
+   geom_smooth(aes(x = transect_location_m, y = rhizome_pwS, color = treatment), method="lm", se=F)
+
+# log transform
+ggplot(meadow) +
+   geom_point(aes(x = transect_location_m, y = log(rhizome_pwS), color = treatment), position = position_jitter(height=0, width=0.07)) + 
+   geom_smooth(aes(x = transect_location_m, y = log(rhizome_pwS), color = treatment), method="lm", se=F)
+
+
+# with separate lines by treatment
+#  all data pooled, separated by site and treatment
+ggplot(meadow) +
+   geom_point(aes(x = transect_location_m, y = rhizome_pwS, color = treatment), position = position_jitter(height=0, width=0.07)) + 
+   geom_smooth(aes(x = transect_location_m, y = rhizome_pwS, color = treatment, group=interaction(treatment, site_name), linetype=site_name), method="lm", se=F)
+
+# log transform
+ggplot(meadow) +
+   geom_point(aes(x = transect_location_m, y = log(rhizome_pwS), color = treatment), position = position_jitter(height=0, width=0.07)) + 
+   geom_smooth(aes(x = transect_location_m, y = log(rhizome_pwS), color = treatment, group=interaction(treatment, site_name), linetype=site_name), method="lm", se=F)
+
+
+
+
 
 # Surface and Rhizome porewater, all patches and separated by treatment
 windows(height=3.5, width=8)
 ggplot(meadow) +
-   # rhizome
-   geom_line(aes(x = transect_location_m, y = rhizome_pwS), 
-             linetype = 1, linewidth= 0.75, alpha=0.5, color="seagreen4") +
-   geom_point(aes(x = transect_location_m, y = rhizome_pwS), size=2, alpha = 0.5, shape=17, color="seagreen4") +
-   # surface
+   # surface - Green
    geom_line(aes(x = transect_location_m, y = surface_pwS), 
+             linetype = 1, linewidth= 0.75, alpha=0.5, color="seagreen4") +
+   geom_point(aes(x = transect_location_m, y = surface_pwS), size=2, alpha = 0.5, shape=22, color="seagreen4") +
+   # rhizome - Blue
+   geom_line(aes(x = transect_location_m, y = rhizome_pwS), 
              linetype = 1, linewidth= 0.75, alpha=0.5, color="cornflowerblue") +
-   geom_point(aes(x = transect_location_m, y = surface_pwS), size=2, alpha = 0.5, shape=22, color="cornflowerblue") +
+   geom_point(aes(x = transect_location_m, y = rhizome_pwS), size=2, alpha = 0.5, shape=17, color="cornflowerblue") +
    # 
    geom_vline(aes(xintercept = 0), linetype=2, color="gray50") +
    geom_hline(aes(yintercept=0), linetype=3, color="gray50") +
@@ -142,6 +236,29 @@ ggplot(meadow) +
    #
    theme_classic() +
    theme(panel.border = element_rect(color="black", fill=NA))
+
+      # log
+      windows(height=3.5, width=8)
+      ggplot(meadow %>% mutate(across(c(surface_pwS, rhizome_pwS), ~log(.)))) +
+         # surface - Green
+         geom_line(aes(x = transect_location_m, y = surface_pwS), 
+                   linetype = 1, linewidth= 0.75, alpha=0.5, color="seagreen4") +
+         geom_point(aes(x = transect_location_m, y = surface_pwS), size=2, alpha = 0.5, shape=22, color="seagreen4") +
+         # rhizome - Blue
+         geom_line(aes(x = transect_location_m, y = rhizome_pwS), 
+                   linetype = 1, linewidth= 0.75, alpha=0.5, color="cornflowerblue") +
+         geom_point(aes(x = transect_location_m, y = rhizome_pwS), size=2, alpha = 0.5, shape=17, color="cornflowerblue") +
+         # 
+         geom_vline(aes(xintercept = 0), linetype=2, color="gray50") +
+         geom_hline(aes(yintercept=0), linetype=3, color="gray50") +
+         #
+         scale_y_continuous(name = expression(ln~Sulfide~(mu*M))) +
+         # scale_shape_manual(values = c('surface' = 21, 'rhizome' = 25)) +
+         facet_grid(rows = vars(treatment),
+                    cols = vars(site_id)) +
+         #
+         theme_classic() +
+         theme(panel.border = element_rect(color="black", fill=NA))
 
 
 
