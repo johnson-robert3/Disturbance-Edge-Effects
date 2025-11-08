769982ed0636face01a17181cd39eed2ad3cc0b4
Robert Johnson <johnson.robert.333@gmail.com>
2025-11-08 15:25:34 -0600

    indent figs that are iterations of ones above them

--- Figure-Scripts/figs_rapid-viewing-data-and-relationships.R
+++ Figure-Scripts/figs_rapid-viewing-data-and-relationships.R
@@ -13,43 +13,56 @@ ggplot(meadow) +
    theme_classic() +
    theme(panel.border = element_rect(color="black", fill=NA))
 
-# log
-windows(height=3.5, width=8)
-ggplot(meadow) +
-   geom_point(aes(x = log(rhizome_pwS), y = log(surface_pwS)), size=3, alpha = 0.5) +
-   # scale_y_continuous(name = expression(Macroalgal~biomass)) +
-   facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
-   theme_classic() +
-   theme(panel.border = element_rect(color="black", fill=NA))
+   # log
+   windows(height=3.5, width=8)
+   ggplot(meadow) +
+      geom_point(aes(x = log(rhizome_pwS), y = log(surface_pwS)), size=3, alpha = 0.5) +
+      # scale_y_continuous(name = expression(Macroalgal~biomass)) +
+      facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
+      theme_classic() +
+      theme(panel.border = element_rect(color="black", fill=NA))
 
-# pooled (by site)
-windows(height=3.5, width=7)
-ggplot(meadow) +
-   geom_point(aes(x = rhizome_pwS, y = surface_pwS, color=site_id), size=3, alpha = 0.5) +
-   # scale_y_continuous(name = expression(Macroalgal~biomass)) +
-   # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
-   theme_classic() +
-   theme(panel.border = element_rect(color="black", fill=NA))
+   # pooled (by site)
+   windows(height=3.5, width=7)
+   ggplot(meadow) +
+      geom_point(aes(x = rhizome_pwS, y = surface_pwS, color=site_id), size=3, alpha = 0.5) +
+      # scale_y_continuous(name = expression(Macroalgal~biomass)) +
+      # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
+      theme_classic() +
+      theme(panel.border = element_rect(color="black", fill=NA))
 
-# pooled (by treatment)
-windows(height=3.5, width=7)
-ggplot(meadow) +
-   geom_point(aes(x = rhizome_pwS, y = surface_pwS, color = treatment), size=3, alpha = 0.5) +
-   # scale_y_continuous(name = expression(Macroalgal~biomass)) +
-   scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
-   # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
-   theme_classic() +
-   theme(panel.border = element_rect(color="black", fill=NA))
+   # pooled (by treatment)
+   windows(height=3.5, width=7)
+   ggplot(meadow) +
+      geom_point(aes(x = rhizome_pwS, y = surface_pwS, color = treatment), size=3, alpha = 0.5) +
+      # scale_y_continuous(name = expression(Macroalgal~biomass)) +
+      scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
+      # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
+      theme_classic() +
+      theme(panel.border = element_rect(color="black", fill=NA))
 
-# log, pooled (by treatment)
-windows(height=3.5, width=7)
-ggplot(meadow) +
-   geom_point(aes(x = log(rhizome_pwS), y = log(surface_pwS), color = treatment), size=3, alpha = 0.5) +
-   # scale_y_continuous(name = expression(Macroalgal~biomass)) +
-   scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
-   # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
-   theme_classic() +
-   theme(panel.border = element_rect(color="black", fill=NA))
+   # log, pooled (by treatment)
+   windows(height=3.5, width=7)
+   ggplot(meadow) +
+      geom_point(aes(x = log(rhizome_pwS), y = log(surface_pwS), color = treatment), size=3, alpha = 0.5) +
+      # scale_y_continuous(name = expression(Macroalgal~biomass)) +
+      scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
+      # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
+      theme_classic() +
+      theme(panel.border = element_rect(color="black", fill=NA))
+
+
+   # log, pooled (by treatment) - switching axes (rhizome on y-axis)
+   windows(height=3.5, width=7)
+   ggplot(meadow) +
+      geom_point(aes(x = log(surface_pwS), y = log(rhizome_pwS), color = treatment), size=3, alpha = 0.5) +
+      geom_smooth(aes(x = log(surface_pwS), y = log(rhizome_pwS), color = treatment), method="lm", se=F) +
+      # scale_y_continuous(name = expression(Macroalgal~biomass)) +
+      scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
+      # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
+      theme_classic() +
+      theme(panel.border = element_rect(color="black", fill=NA))
+
 
 
 
@@ -63,15 +76,15 @@ ggplot(meadow) +
    theme_classic() +
    theme(panel.border = element_rect(color="black", fill=NA))
 
-# log
-windows(height=3.5, width=7)
-ggplot(meadow) +
-   geom_point(aes(x = perc_om, y = log(surface_pwS), color = treatment), size=3, alpha = 0.5) +
-   # scale_y_continuous(name = expression(Macroalgal~biomass)) +
-   scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
-   # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
-   theme_classic() +
-   theme(panel.border = element_rect(color="black", fill=NA))
+   # log
+   windows(height=3.5, width=7)
+   ggplot(meadow) +
+      geom_point(aes(x = perc_om, y = log(surface_pwS), color = treatment), size=3, alpha = 0.5) +
+      # scale_y_continuous(name = expression(Macroalgal~biomass)) +
+      scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
+      # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
+      theme_classic() +
+      theme(panel.border = element_rect(color="black", fill=NA))
 
 
 
@@ -85,15 +98,15 @@ ggplot(meadow) +
    theme_classic() +
    theme(panel.border = element_rect(color="black", fill=NA))
 
-# log
-windows(height=3.5, width=7)
-ggplot(meadow) +
-   geom_point(aes(x = perc_om, y = log(rhizome_pwS), color = treatment), size=3, alpha = 0.5) +
-   # scale_y_continuous(name = expression(Macroalgal~biomass)) +
-   scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
-   # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
-   theme_classic() +
-   theme(panel.border = element_rect(color="black", fill=NA))
+   # log
+   windows(height=3.5, width=7)
+   ggplot(meadow) +
+      geom_point(aes(x = perc_om, y = log(rhizome_pwS), color = treatment), size=3, alpha = 0.5) +
+      # scale_y_continuous(name = expression(Macroalgal~biomass)) +
+      scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
+      # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
+      theme_classic() +
+      theme(panel.border = element_rect(color="black", fill=NA))
 
 
 
@@ -107,15 +120,15 @@ ggplot(meadow) +
    theme_classic() +
    theme(panel.border = element_rect(color="black", fill=NA))
 
-# log
-windows(height=3.5, width=7)
-ggplot(meadow) +
-   geom_point(aes(x = porosity, y = log(surface_pwS), color = treatment), size=3, alpha = 0.5) +
-   # scale_y_continuous(name = expression(Macroalgal~biomass)) +
-   scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
-   # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
-   theme_classic() +
-   theme(panel.border = element_rect(color="black", fill=NA))
+   # log
+   windows(height=3.5, width=7)
+   ggplot(meadow) +
+      geom_point(aes(x = porosity, y = log(surface_pwS), color = treatment), size=3, alpha = 0.5) +
+      # scale_y_continuous(name = expression(Macroalgal~biomass)) +
+      scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
+      # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
+      theme_classic() +
+      theme(panel.border = element_rect(color="black", fill=NA))
 
 
 
@@ -129,15 +142,15 @@ ggplot(meadow) +
    theme_classic() +
    theme(panel.border = element_rect(color="black", fill=NA))
 
-# log
-windows(height=3.5, width=7)
-ggplot(meadow) +
-   geom_point(aes(x = porosity, y = log(rhizome_pwS), color = treatment), size=3, alpha = 0.5) +
-   # scale_y_continuous(name = expression(Macroalgal~biomass)) +
-   scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
-   # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
-   theme_classic() +
-   theme(panel.border = element_rect(color="black", fill=NA))
+   # log
+   windows(height=3.5, width=7)
+   ggplot(meadow) +
+      geom_point(aes(x = porosity, y = log(rhizome_pwS), color = treatment), size=3, alpha = 0.5) +
+      # scale_y_continuous(name = expression(Macroalgal~biomass)) +
+      scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
+      # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
+      theme_classic() +
+      theme(panel.border = element_rect(color="black", fill=NA))
 
 
 
@@ -150,14 +163,14 @@ ggplot(meadow) +
    theme_classic() +
    theme(panel.border = element_rect(color="black", fill=NA))
 
-# log
-windows(height=3.5, width=7)
-ggplot(meadow) +
-   geom_point(aes(x = burrow_density, y = log(surface_pwS), color = treatment), size=3, alpha = 0.5) +
-   scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
-   # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
-   theme_classic() +
-   theme(panel.border = element_rect(color="black", fill=NA))
+   # log
+   windows(height=3.5, width=7)
+   ggplot(meadow) +
+      geom_point(aes(x = burrow_density, y = log(surface_pwS), color = treatment), size=3, alpha = 0.5) +
+      scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
+      # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
+      theme_classic() +
+      theme(panel.border = element_rect(color="black", fill=NA))
 
 
 
@@ -171,15 +184,15 @@ ggplot(meadow) +
    theme_classic() +
    theme(panel.border = element_rect(color="black", fill=NA))
 
-# log
-windows(height=3.5, width=7)
-ggplot(meadow) +
-   geom_point(aes(x = burrow_density, y = log(rhizome_pwS), color = treatment), size=3, alpha = 0.5) +
-   # scale_y_continuous(name = expression(Macroalgal~biomass)) +
-   scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
-   # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
-   theme_classic() +
-   theme(panel.border = element_rect(color="black", fill=NA))
+   # log
+   windows(height=3.5, width=7)
+   ggplot(meadow) +
+      geom_point(aes(x = burrow_density, y = log(rhizome_pwS), color = treatment), size=3, alpha = 0.5) +
+      # scale_y_continuous(name = expression(Macroalgal~biomass)) +
+      scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
+      # facet_wrap(facets = vars(site_id), nrow=2, scales="free") +
+      theme_classic() +
+      theme(panel.border = element_rect(color="black", fill=NA))
 
 
 
@@ -224,14 +237,13 @@ ggplot(meadow %>%
    scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
    theme_classic()
    
-   
-# log Surface sulfide vs burrow density (trt/dist means)
-ggplot(meadow %>%
-          mutate(surface_pwS = log(surface_pwS)) %>%
-          summarize(across(c(surface_pwS, burrow_density), ~mean(., na.rm=T)), .by = c(treatment, distance))) +
-   geom_point(aes(x = burrow_density, y = surface_pwS, color = treatment), size=3, alpha = 0.5) +
-   scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
-   theme_classic()
+   # log Surface sulfide vs burrow density (trt/dist means)
+   ggplot(meadow %>%
+             mutate(surface_pwS = log(surface_pwS)) %>%
+             summarize(across(c(surface_pwS, burrow_density), ~mean(., na.rm=T)), .by = c(treatment, distance))) +
+      geom_point(aes(x = burrow_density, y = surface_pwS, color = treatment), size=3, alpha = 0.5) +
+      scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
+      theme_classic()
 
 
 # Rhizome sulfide vs burrow density (trt/dist means)
@@ -241,16 +253,15 @@ ggplot(meadow %>%
    scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
    theme_classic()
 
-
-# log Rhizome sulfide vs burrow density (trt/dist means)
-ggplot(meadow %>%
-          mutate(rhizome_pwS = log(rhizome_pwS)) %>%
-          summarize(across(c(rhizome_pwS, burrow_density), ~mean(., na.rm=T)), .by = c(treatment, distance))) +
-   geom_point(aes(x = burrow_density, y = rhizome_pwS, color = treatment), size=3, alpha = 0.5) +
-   scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
-   lims(x = c(0,60), y = c(3,5)) +
-   theme_classic()
-
+   # log Rhizome sulfide vs burrow density (trt/dist means)
+   ggplot(meadow %>%
+             mutate(rhizome_pwS = log(rhizome_pwS)) %>%
+             summarize(across(c(rhizome_pwS, burrow_density), ~mean(., na.rm=T)), .by = c(treatment, distance))) +
+      geom_point(aes(x = burrow_density, y = rhizome_pwS, color = treatment), size=3, alpha = 0.5) +
+      scale_color_manual(values = c("vegetated" = "#41A67E", "unvegetated" = "#05339C")) +
+      lims(x = c(0,60), y = c(3,5)) +
+      theme_classic()
+   
 
 
 
